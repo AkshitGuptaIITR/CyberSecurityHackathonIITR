43f2b8ef88bbd883fb453d834ce4b4cd0a369a79
Gauri Pachauri <gauri_p@ece.iitr.ac.in>
2022-05-13 04:12:04 +0530

    Adding files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import Footer from "./Components/Footer/Footer";
 import Header from "./Components/Header/Header";
+import ShowCase from "./Components/ShowCase/ShowCase";
+import About from "./Components/About/About";
+import Speaker from "./Components/Speakers/Speaker";
 
 function App() {
   return (
     <>
       <Header />
+      <ShowCase />
+      <About />
+      <Speaker />
       <Footer />
     </>
   );
---
+++ src/Components/Speakers/Speaker.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import style from "./Speaker.module.css";
+
+const Speaker = () => {
+    return (
+        <>
+            <div className={style.speaker}>
+                <h2>KEYNOTE SPEAKERS</h2>
+            </div>
+        </>
+    )
+}
+
+export default Speaker;
